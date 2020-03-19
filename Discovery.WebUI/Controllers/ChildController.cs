13261435c90ff286d8f2f6d70704e361f69fd89e
MarShallOwn <53130198+MarShallOwn@users.noreply.github.com>
2020-03-19 10:16:36 +0200

    Child Table Sorting and Search using Ajax

--- Discovery.WebUI/Controllers/ChildController.cs
+++ Discovery.WebUI/Controllers/ChildController.cs
@@ -14,17 +14,78 @@ namespace Discovery.WebUI.Controllers
         DataContext context = new DataContext();
 
         // GET: Child
-        public ActionResult Index()
+        public ActionResult Index(string sortOrder)
         {
+
+            ViewBag.name = String.IsNullOrEmpty(sortOrder) ? "name_desc" : "";
+            ViewBag.lastName = sortOrder == "LastN" ? "LastN_desc" : "LastN";
+            ViewBag.age = sortOrder == "Age" ? "Age_desc" : "Age";
+            ViewBag.grade = sortOrder == "Grade" ? "Grade_desc" : "Grade";
+            ViewBag.disability_type = sortOrder == "Disability_Type" ? "Disability_Type_desc" : "Disability_Type";
+            ViewBag.classroom = sortOrder == "Classroom" ? "Classroom_desc" : "Classroom";
+
             List<Child> children = context.Children.ToList();
-            
+
+            switch (sortOrder)
+            {
+                case "name_desc":
+                    children = children.OrderByDescending(c => c.FirstName).ToList();
+                    break;
+                case "LastN":
+                    children = children.OrderBy(c => c.LastName).ToList();
+                    break;
+                case "LastN_desc":
+                    children = children.OrderByDescending(c => c.LastName).ToList();
+                    break;
+                case "Age":
+                    children = children.OrderBy(c => c.Age).ToList();
+                    break;
+                case "Age_desc":
+                    children = children.OrderByDescending(c => c.Age).ToList();
+                    break;
+                case "Grade":
+                    children = children.OrderBy(c => c.Grade).ToList();
+                    break;
+                case "Grade_desc":
+                    children = children.OrderByDescending(c => c.Grade).ToList();
+                    break;
+                case "Disability_Type":
+                    children = children.OrderBy(c => c.Disability_Type).ToList();
+                    break;
+                case "Disability_Type_desc":
+                    children = children.OrderByDescending(c => c.Disability_Type).ToList();
+                    break;
+                case "Classroom":
+                    children = children.OrderBy(c => c.ClassRoom).ToList();
+                    break;
+                case "Classroom_desc":
+                    children = children.OrderByDescending(c => c.ClassRoom).ToList();
+                    break;
+                default:
+                    children = children.OrderBy(c => c.FirstName).ToList();
+                    break;
+            }
+
             return View(children);
         }
 
         [HttpPost]
-        public JsonResult doSearch(string Search)
+        public JsonResult doSearch(string Search, string Option)
         {
-            List<Child> children = context.Children.Where(c => c.FirstName.Contains(Search)).ToList();
+            List<Child> children;
+            switch (Option)
+            {
+                case "option2":
+                    children = context.Children.Where(c => c.LastName.StartsWith(Search)).ToList();
+                    break;
+                case "option3":
+                    children = context.Children.Where(c => c.Disability_Type.StartsWith(Search)).ToList();
+                    break;
+                default:
+                    children = context.Children.Where(c => c.FirstName.StartsWith(Search)).ToList();
+                    break;
+            }
+            
 
             return Json(children, JsonRequestBehavior.AllowGet);
         }
