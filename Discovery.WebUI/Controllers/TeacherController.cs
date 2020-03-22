a2c981b199ea9667529de347268370557acb5200
marwa593 <marwa.mamm79@gmail.com>
2020-03-22 20:29:01 +0200

    Teacher Controller

---
+++ Discovery.WebUI/Controllers/TeacherController.cs
@@ -0,0 +1,144 @@
+﻿using Discovery.Core.Models;
+using Discovery.DataAccess.SQL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Discovery.WebUI.Controllers
+{
+    public class TeacherController : Controller
+    {
+        DataContext context = new DataContext();
+
+        // GET: Teacher
+
+        public ActionResult Index()
+        {
+            List<Teacher> Teachers = context.Teachers.ToList();
+            return View(Teachers);
+        }
+       
+        public ActionResult Create()
+        {
+            Teacher t = new Teacher();
+            return View(t);
+        }
+        [HttpPost]
+        public ActionResult Create(Teacher t)
+        {
+           if(!ModelState.IsValid)
+           {
+                return View(t);
+
+           }
+            else
+            {
+                context.Teachers.Add(t);
+                context.SaveChanges();
+                return RedirectToAction("index");
+            }
+
+        }
+        [HttpGet]
+        public ActionResult Edit(string Id)
+        {
+            Teacher t = context.Teachers.Find(Id);
+            if (t == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                return View(t);
+            }
+        }
+        [HttpPost]
+        public ActionResult Edit(Teacher t ,string Id)
+        {
+            Teacher te = context.Teachers.Find(Id);
+            if(te == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                if(!ModelState.IsValid)
+                {
+                    return View(t);
+
+                }
+                else
+                {
+                    te.FirstName = t.FirstName;
+                    te.LastName = t.LastName;
+                    te.PhoneNumber = t.PhoneNumber;
+                    te.Email= t.Email;
+                    te.ClassRoom = t.ClassRoom;
+
+                    context.SaveChanges();
+
+                    return RedirectToAction("Index");
+
+                }
+
+
+            }
+                
+        }
+        public ActionResult Details(string Id)
+        {
+            Teacher t = context.Teachers.Find(Id);
+           if(t == null)
+           {
+                return HttpNotFound();
+           }
+           else
+            {
+                return View(t);
+            }
+        }
+        [HttpGet]
+        public ActionResult Delete(string Id)
+        {
+            Teacher t = context.Teachers.Find(Id);
+            if (t == null)
+            {
+                return HttpNotFound();
+
+            }
+            else
+            {
+                return View(t);
+            }
+        }
+        [HttpPost]
+        [ActionName("Delete")]
+        public ActionResult cDelete(string Id)
+        {
+            Teacher t= context.Teachers.Find(Id);
+            if(t == null)
+            {
+                return HttpNotFound();
+            }
+            else
+            {
+                context.Teachers.Remove(t);
+                context.SaveChanges();
+
+                return RedirectToAction("Index");
+
+            }
+        }
+        
+
+        
+
+        
+         
+
+        
+    }
+        
+}
